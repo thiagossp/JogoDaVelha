2a5be66b3440e7ad9fdc3e0b6a71847a3562c0bf
Thiago da Silva Pinto <thiagosp@daebauru.sp.gov.br>
2019-04-12 09:33:37 -0300

    Initial Commit

---
+++ UnitMain.pas
@@ -0,0 +1,24 @@
+unit UnitMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFormMain = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
